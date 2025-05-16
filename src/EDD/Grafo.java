d261ca75fa1a9ee139dcbade30e21959ff99d2a6
Manzanaverde097 <Vyckhysar097@gmail.com>
2025-05-16 07:33:15 -0600

    completa el grafo :sparkles:

--- src/EDD/Grafo.java
+++ src/EDD/Grafo.java
@@ -11,6 +11,12 @@ public class Grafo {
       this.numVertices = 16;
       this.matrizAdyacencia = new int[this.numVertices][this.numVertices];
       this.vertices = new Vertice[this.numVertices];
+      
+        for (int i = 0; i < this.numVertices; i++) {
+            for (int j = 0; j < this.numVertices; j++) {
+                this.matrizAdyacencia[i][j] = 0;
+            }
+        }
     }
 
     public int getNumVertices() {
@@ -35,5 +41,74 @@ public class Grafo {
 
     public void setVertices(Vertice[] vertices) {
         this.vertices = vertices;
+    }
+    
+    public void insertar(String letra){
+        Vertice vertice = new Vertice(letra);
+        if(this.indiceVacio() != -1){
+            this.vertices[this.indiceVacio()] = vertice;
+        }else{
+            System.out.println("El aareglo esta lleno");
+        }
+    }
+    
+    private int indiceVacio(){
+        for (int i = 0; i < this.vertices.length; i++) {
+            if(vertices[i] == null){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public void conectarVertice(String letra1, String letra2){
+        if(this.buscarVertice(letra1) != -1 && this.buscarVertice(letra2) != -1){
+            int i = this.buscarVertice(letra1);
+            int j = this.buscarVertice(letra2);
+            this.matrizAdyacencia[i][j] = 1;
+            this.matrizAdyacencia[j][i] = 1;
+        }else{
+            System.out.println("Alguna de las letras no se encuentra en el grafo.");
+        }
+    }
+    
+    public int buscarVertice(String letra){
+        for (int i = 0; i < this.vertices.length; i++) {
+            if(vertices[i] != null){
+                if(vertices[i].getLetra().equalsIgnoreCase(letra)){
+                    return i;
+                }
+            }
+        }   
+        return -1;
+    }
+    
+    public int contarVertices(){
+        int count = 0;
+        for (int i = 0; i < this.numVertices; i++) {
+            if(vertices[i] != null){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    public void asignarCoordenadas(){
+        int cantidadVertices = this.contarVertices();
+        if(cantidadVertices == 16){
+            int count = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    Vertice verticeActual = vertices[count];
+                    verticeActual.setFila(i);
+                    verticeActual.setColumna(j);
+                    count++;
+                }
+            }
+        }else{
+            System.out.println("No estan los 16 vertices. Hay " + cantidadVertices);
+        }
     }
 }
